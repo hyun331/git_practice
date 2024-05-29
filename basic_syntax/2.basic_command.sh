e7738de0d967bbd2ba54283227296216ad89c8a2
hyun331 <sus03319@gmail.com>
2024-05-29 15:20:55 +0900

    fix

--- basic_syntax/2.basic_command.sh
+++ basic_syntax/2.basic_command.sh
@@ -3,9 +3,6 @@ git status
 
 # git add . : 모든 수정/추가사사항 add
 
-# commit 이력 확인, 포함. (local repository 이력)
-git log
-
 # git add 특정파일 : 특정파일 add
 git ad 특특정파일(위치포함)
 
@@ -14,6 +11,19 @@ git comit -m "메세지 타이틀" -m "메세지 내용"
 
 # vi모드에서 작성 : git commit후 엔터
 
-dd
+# add 와 commit 동시에
+git commit -am "add와 commit 동시에"
 
     
+# commit 이력 확인, 포함. (local repository 이력)
+git log
+# git log 간단하게
+git log --oneline
+# 전체 이력 조회
+git log --all
+
+# 원격 저장소로 업로드
+git push origin 브랜치명
+
+# 충돌 무시하고 강제 push
+git push origin 브랜드명 
