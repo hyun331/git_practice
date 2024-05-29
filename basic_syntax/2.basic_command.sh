a74acc130872efa0a40bf8fa532d451e4eb5075e
hyun331 <sus03319@gmail.com>
2024-05-29 15:02:29 +0900

    vi commit this is vi commit

---
+++ basic_syntax/2.basic_command.sh
@@ -0,0 +1,19 @@
+# 현재 working directory, staging area만 상태 확인
+git status
+
+# git add . : 모든 수정/추가사사항 add
+
+# commit 이력 확인, 포함. (local repository 이력)
+git log
+
+# git add 특정파일 : 특정파일 add
+git ad 특특정파일(위치포함)
+
+# local repository에 반영 및 커밋 이력 생성
+git comit -m "메세지 타이틀" -m "메세지 내용"
+
+# vi모드에서 작성 : git commit후 엔터
+
+
+
+    
